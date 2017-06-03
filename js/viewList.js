0d23a3dc2289bec3a411a33c4db79bbba939deed
Anthony Z <anthony.zamarro@gmail.com>
2017-06-03 15:05:55 -0400

    Get college/university wiki-api data

--- js/viewList.js
+++ js/viewList.js
@@ -30,22 +30,22 @@ var array = [
 var ViewModel = function () {
 var self = this;
 
+var item;
 this.viewList = ko.observableArray([]);
   array.forEach(function(listItem){
     self.viewList.push(listItem);
   });
 
-this.list = ko.observable(self.viewList()[0])
+this.list = ko.observable(self.viewList()[0]);
 
-
-this.url = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=list+of+colleges+and+universities+in+metropolitan+boston&namespace=0&limit=10';
+this.url = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=1&titles=List_of_colleges_and_universities_in_metropolitan_Boston';
 
      $.ajax({
        url: this.url,
-       data: { action: 'opensearch', format: 'json'},
+       data: { action: 'query', format: 'json'},
        dataType: 'jsonp',
        success: function (x) {
-         console.log(x);
+         console.log(x.query.pages[402352].revisions[0]['*']);
        }
 })
 }
