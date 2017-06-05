211c3baa4da85628e30977870c1700f65a6b6232
Anthony Z <anthony.zamarro@gmail.com>
2017-06-05 11:39:13 -0400

    Update ajax data parameters for Foursquare API

--- js/app.js
+++ js/app.js
@@ -1,22 +1,31 @@
 //Use Foursquare API to get restaurant data to populate model
-var url = 'https://api.foursquare.com/v2/venues/search?client_id=MU5LQAECHVZLCEYZGSXIZ3BWLAQ5HZP3BRRHCRL1YJ1WJTST%20&client_secret=EDFVN04UNKLC0FRNS20ORZPZJRTVIF4XAHDCMVCI2HGC1NTT%20&near=boston&query=restaurants%20&v=20200131%20&m=foursquare';
+// var url = 'https://api.foursquare.com/v2/venues/search?client_id=MU5LQAECHVZLCEYZGSXIZ3BWLAQ5HZP3BRRHCRL1YJ1WJTST%20&client_secret=EDFVN04UNKLC0FRNS20ORZPZJRTVIF4XAHDCMVCI2HGC1NTT%20&near=boston&query=restaurants%20&v=20200131%20&m=foursquare';
+var url = 'https://api.foursquare.com/v2/venues/search';
 var name, hours, location, price, category;
 var model =
 [
   {
   foursquareData: $.ajax({
    url: url,
-   dataType: 'jsonp',
-   success: function (x) {
-     console.log(x);
-     var response = x.response.venues
-     for (var i = 0; i < response.length; i++) {
-       name = response[i].name;
-       category = response[i].category;
-       location = response[i].location;
-       hours = response[i].hours;
-       price = response[i].price;
-     }
+   dataType: 'json',
+   data: {
+     client_id: "MU5LQAECHVZLCEYZGSXIZ3BWLAQ5HZP3BRRHCRL1YJ1WJTST",
+     client_secret: "EDFVN04UNKLC0FRNS20ORZPZJRTVIF4XAHDCMVCI2HGC1NTT",
+     v: 20170604,
+     near: "boston",
+     query: "restaurants",
+     async: true
+   },
+   success: function (data) {
+     console.log(data);
+    //  var response = data.response.venues
+    //  for (var i = 0; i < response.length; i++) {
+    //    name = response[i].name;
+    //    category = response[i].category;
+    //    location = response[i].location;
+    //    hours = response[i].hours;
+    //    price = response[i].price;
+    //  }
    },
    error: function(e) {
      alert('Sorry! Data unavailable at this time. Please refresh the page and try again.');
@@ -29,7 +38,6 @@ var model =
 function ViewModel() {
 var self = this;
 // console.log(model);
-restaurant()
 
 }
 
@@ -45,13 +53,13 @@ function initMap() {
   mapTypeControl: false
 });
 var locations = [
-  {title: 'Harvard University', location: {lat: 42.377003, lng:-71.116660}},
-  {title: 'Massachusetts Institute of Technology', location: {lat: 42.360091, lng:-71.094160}},
-  {title: 'Northeastern University', location: {lat: 42.339807, lng:-71.089172}},
-  {title: 'Boston University', location: {lat: 42.350500, lng:-71.105399}},
-  {title: 'Boston College', location: {lat: 42.335549, lng:-71.168495}},
-  {title: 'Suffolk University', location: {lat: 42.358520, lng:-71.061356}},
-  {title: 'Emerson College', location: {lat: 42.352030, lng:-71.065655}}
+  {title: 'Five Guys', location: {lat: 42.377003, lng:-71.116660}},
+  {title: 'Roxy\'s', location: {lat: 42.360091, lng:-71.094160}},
+  {title: 'Amelia\'s Tanqueria', location: {lat: 42.339807, lng:-71.089172}},
+  {title: 'Supper 88', location: {lat: 42.350500, lng:-71.105399}},
+  {title: 'Eagle\s Deli', location: {lat: 42.335549, lng:-71.168495}},
+  {title: 'Highspot Deli', location: {lat: 42.358520, lng:-71.061356}},
+  {title: 'New York Pizza', location: {lat: 42.352030, lng:-71.065655}}
 ]
   //Create markers for each location
   for (var i = 0; i < locations.length; i++) {
