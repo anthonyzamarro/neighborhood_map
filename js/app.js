bdb079695d4d26af29d4b37201e8fd4bd77127c0
Anthony Z <anthony.zamarro@gmail.com>
2017-06-07 23:44:59 -0400

    Filter view list and add to infowindows

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //Use Foursquare API to get restaurant data to populate model
 // var url = 'https://api.foursquare.com/v2/venues/search?client_id=MU5LQAECHVZLCEYZGSXIZ3BWLAQ5HZP3BRRHCRL1YJ1WJTST%20&client_secret=EDFVN04UNKLC0FRNS20ORZPZJRTVIF4XAHDCMVCI2HGC1NTT%20&near=boston&query=restaurants%20&v=20200131%20&m=foursquare';
 var url = 'https://api.foursquare.com/v2/venues/search'
-var response, name, contact, address, url, type, lat, lng, vm, map, marker;
+var response, name, contact, address, url, type, lat, lng, vm, map, marker, visible, listName;
 
 //Constructor function to create Restaurant info
 var Restaurant = function (data) {
@@ -16,17 +16,16 @@ var Restaurant = function (data) {
 }
 
 //Create map and use Foursquare data to get locations and restaurant details
- map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
   center: {lat: 42.355709, lng: -71.057205},
   zoom: 13,
   mapTypeControl: false
 });
-
- vm = new ViewModel();
+vm = new ViewModel();
 
 ko.applyBindings(vm);
+
 
 
 $.ajax({
@@ -39,7 +38,7 @@ $.ajax({
    near: "boston",
    query: "restaurant",
    async: true,
-   limit: 15
+  //  limit: 15
  },
  success: function (data) {
    response = data.response.venues;
@@ -66,31 +65,49 @@ $.ajax({
         map: map,
         animation: google.maps.Animation.DROP
       });
+       listName = marker.name;
 
        marker.addListener('click', function(){
-        populateInfoWindow(this, infowindow);
+        populateInfoWindow(markerData, infowindow);
       });
 
       bounds.extend(marker.position);
   }
 
-    function populateInfoWindow(marker, infowindow) {
-        infowindow.setContent('<h3>' + marker.name + '</h3>' + '<br />');
+    function populateInfoWindow(markerData, infowindow) {
+      makeMarkers(markerData);
+      marker.setAnimation(google.maps.Animation.NULL);
+        infowindow.setContent('<h3>' + markerData.name + '</h3>' + '<br />' +
+                              '<div>' + markerData.address + '</div>' +
+                              '<div><a target="_blank" href="' + markerData.url + '">' + 'Visit their website' + '</div>');
         infowindow.open(map, marker);
-    };
-
+      };
   //Use Foursquare data to populate the list
   function ViewModel(restaurantData) {
       var self = this;
-
+      this.markerName = listName;
       this.restaurant = ko.observableArray([]);
+      this.searchList = document.getElementById('search-box');
+      this.searchList = ko.observable()
 
+      //Show infowindow when user clicks restaurant in list view
       this.restaurantClick = function (infowindowData) {
         makeMarkers(infowindowData);
         marker.setAnimation(google.maps.Animation.NULL);
-        infowindow.setContent('<h3>' + marker.name + '</h3>' + '<br />');
+        infowindow.setContent('<h3>' + infowindowData.name + '</h3>' + '<br />' +
+                              '<div>' + infowindowData.address + '</div>' +
+                              '<div><a target="_blank" href="' + infowindowData.url + '">' + 'Visit their website' + '</div>');
         infowindow.open(map, marker);
-        }
-    }
+      };
 
+      this.filter = ko.computed(function() {
+        self.searchList()
+        
+      });
+
+
+      this.searchBtn = function () {
+
+      }
+    }
 }
